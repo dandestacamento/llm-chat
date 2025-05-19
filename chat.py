6add65898656c37d207ac6f21954446bc31b6497
dandestacamento <dandestacamento@gmail.com>
2025-05-19 01:13:02 +0000

    Initial Chatbot script

---
+++ chat.py
@@ -0,0 +1,55 @@
+from typing import List
+from ctransformers import AutoModelForCausalLM
+import chainlit as cl
+
+
+def get_prompt(instruction: str, history: List[str] = None) -> str:
+    system = "You are an AI assistant that gives helpful answers. You answer the questions in short and concise way."
+    prompt = f"### System:\n{system}\n\n### User:\n"
+    if len(history) > 0:
+        prompt += f"This is the conversation history: {''.join(history)}. Now answer the question: "
+    prompt += f"{instruction}\n\n### Response:\n"
+    return prompt
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    message_history = cl.user_session.get("message_history")
+    msg = cl.Message(content="")
+    await msg.send()
+
+    prompt = get_prompt(message.content, message_history)
+    response = ""
+    for word in llm(prompt, stream=True):
+        await msg.stream_token(word)
+        response += word
+    await msg.update()
+    message_history.append(response)
+
+
+@cl.on_chat_start
+def on_chat_start():
+    cl.user_session.set("message_history", [])
+    global llm
+    llm = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7B-Chat-GGUF", model_file="llama-2-7b-chat.Q5_K_M.gguf")
+
+
+"""
+history = []
+
+question = "Which city is the capital of India?"
+
+answer = ""
+for word in llm(get_prompt(question), stream=True):
+    print(word, end="", flush=True)
+    answer += word
+print()
+
+history.append(answer)
+
+question = "And which is of the United States?"
+
+for word in llm(get_prompt(question, history), stream=True):
+    print(word, end="", flush=True)
+print()
+"""
